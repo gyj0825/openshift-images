308372e127aca58a0f59874495b5d9d6ce9b74ee
yuejun <yuguo@redhat.com>
2016-09-24 11:58:01 -0400

    bug fix

--- patch-custom-build/custombuild.sh
+++ patch-custom-build/custombuild.sh
@@ -28,9 +28,43 @@ if [ -n "${OUTPUT_IMAGE}" ]; then
   TAG="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
 fi
 
+if [[ -d /var/run/secrets/openshift.io/push ]] && [[ ! -e /root/.dockercfg ]]; then
+  cp /var/run/secrets/openshift.io/push/.dockercfg /root/.dockercfg
+fi
+
 echo "start patch image......"
 echo $BUILD
 
+###get base image
+#if NAMESPACE is't openshift,you must add policy.
+if [ ! -n "$NAMESPACE" ];then
+  namespace=`cat /var/run/secrets/kubernetes.io/serviceaccount/namespace`
+else
+  namespace=$NAMESPACE
+fi
+
+# support imagestream and image
+if [ ! -n "$BASE_PATCH_IMAGE" ];then
+  echo "Error: please provide base image to builder."
+  exit 1
+fi
+
+echo $BASE_PATCH_IMAGE | grep -q "/"
+# decide image or imagstream
+if [ $? != 0 ];then
+   build_base_image=${OUTPUT_REGISTRY}/${namespace}/${BASE_PATCH_IMAGE}:latest
+else
+   build_base_image=$BASE_PATCH_IMAGE
+fi
+echo "pacth image: {'image': '$build_base_image'}"
+
+docker pull "$build_base_image"
+if [ $? != 0 ];then
+  echo "Error: try pull $build_base_image failed."
+  exit 1
+fi
+
+###whether patch source URL is null
 if [[ -n "${SOURCE_REPOSITORY}" ]]; then
   URL="${SOURCE_REPOSITORY}"
   curl --head --silent --fail --location --max-time 16 $URL > /dev/null
@@ -40,8 +74,8 @@ if [[ -n "${SOURCE_REPOSITORY}" ]]; then
   fi
 fi
 
+### docker build or docker tag
 BUILD_DIR=$(mktemp --directory)
-
 # download patch packages
 PKG_NAME=`basename $URL`
 echo ">> Downloading Patch packages... "
@@ -53,40 +87,19 @@ if [ $? != 0 ]; then
 fi
 pushd "${BUILD_DIR}"
 
-#if NAMESPACE is't openshift,you must add policy.
-if [ ! -n "$NAMESPACE" ];then
-  namespace=`cat /var/run/secrets/kubernetes.io/serviceaccount/namespace`
-else
-  namespace=$NAMESPACE
-fi
-
-#use API get imagestream status 
-#TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
-#base_image_name=`curl -s  -H "Authorization: Bearer $TOKEN" --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt  https://openshift.default.svc.cluster.local/oapi/v1/namespaces/$namespace/imagestreams/$IMAGESTREAM | grep dockerImageRepository | awk -F '"' '{print $4}'`
-#base_image_tag=latest
-#base_image=$base_image_name:$base_image_tag
-
-# support imagestream and image
-if [ ! -n "$BASE_PATCH_IMAGE" ];then
-  echo "Error: please provide base image to builder"
-  exit 1
-fi
-
-echo $BASE_PATCH_IMAGE | grep -q "/" 
-# decide image or imagstream
-if [ $? != 0 ];then
-   build_base_image=${OUTPUT_REGISTRY}/${namespace}/${BASE_PATCH_IMAGE}:latest
-else
-   build_base_image=$BASE_PATCH_IMAGE
-fi
-echo "pacth image: {'image': '$build_base_image'}"
-
 # get patch image user id
-RUN_UID=`docker inspect $build_base_image | grep  -E "\"User\": \"[0-9]+\"" | sed -n 1p | awk -F '"' '{print $4}'`
-
 if [ ! -n "$RUN_UID" ];then
-   echo "Error: $build_base_image do not assign User"
-   exit 1 
+  run_uid=`docker inspect $build_base_image | grep  -E "\"User\": \"[0-9]+\"" | sed -n 1p | awk -F '"' '{print $4}'`
+  if [ ! -n "$run_uid" ];then
+     echo "Error: $build_base_image do not assign User,please assign user id by RUN_UID Environment variables"
+     exit 1 
+  fi
+else
+  run_uid="${RUN_UID}"
+  if [ "$run_uid" == "0" ];then
+     echo "Error: can't use super user root."
+     exit 1
+  fi
 fi
 
 #define patch command,default unzip ,you can use "tar -zxf","rpm -ivh" etc
@@ -106,7 +119,7 @@ if [ -n "$RUN_COMMAND" ];then
 fi
 
 # generated Dockerfile
-dockerfile="FROM $build_base_image\nCOPY ${PKG_NAME} /tmp/${PKG_NAME}\nUSER 0\nWORKDIR /\nRUN  ${run_command}\nUSER $RUN_UID"
+dockerfile="FROM $build_base_image\nCOPY ${PKG_NAME} /tmp/${PKG_NAME}\nUSER 0\nWORKDIR /\nRUN  ${run_command}\nUSER $run_uid"
 
 if [ -n "$CUSTOM_DOCKERFILE" ];then
   echo "using custom Dockerfile to build......"
@@ -117,10 +130,6 @@ echo -e "${dockerfile}" > Dockerfile
 
 docker build --rm -t "${TAG}" "${BUILD_DIR}"
 popd
-
-if [[ -d /var/run/secrets/openshift.io/push ]] && [[ ! -e /root/.dockercfg ]]; then
-  cp /var/run/secrets/openshift.io/push/.dockercfg /root/.dockercfg
-fi
 
 if [ -n "${OUTPUT_IMAGE}" ] || [ -s "/root/.dockercfg" ]; then
   docker push "${TAG}"
